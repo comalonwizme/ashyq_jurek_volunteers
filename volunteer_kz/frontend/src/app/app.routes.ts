ab357fe5ea3add39a080ff929725cb7a8eca6a99
abilmansur <aliaidarmm0@gmail.com>
2026-04-20 03:35:03 +0500

    update3

--- volunteer_kz/frontend/src/app/app.routes.ts
+++ volunteer_kz/frontend/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from '../components/home/home';
+import { Login } from '../components/login/login';
+import { Registration } from '../components/registration/registration';
+import { Projects } from '../components/projects/projects';
+import { Ratings } from '../components/ratings/ratings';
+import { Problems } from '../components/problems/problems';
 
 export const routes: Routes = [
     {path: '', redirectTo: "home", pathMatch: "full"},
-    {path: 'home', component: Home}
+    {path: 'home', component: Home},
+    {path: 'login', component: Login},
+    {path: "register", component: Registration},
+    {path: "projects", component: Projects},
+    {path: 'ratings', component: Ratings},
+    {path: "problems", component: Problems}
 ];

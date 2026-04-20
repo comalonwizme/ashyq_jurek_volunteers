4c3a3299b42e728ed270d50e5c6155469e83de01
abilmansur <aliaidarmm0@gmail.com>
2026-04-20 08:36:17 +0500

    updating_commit

--- volunteer_kz/backend/api/models.py
+++ volunteer_kz/backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class VolunteerProfile(models.Model):
+    ROLE_CHOICES = [
+        ("volunteer", "Volunteer"),
+        ("coordinator", "Coordinator")
+    ]
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    iin = models.CharField(max_length=12)
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default="volunteer")
+    profile_image = models.ImageField(upload_to="profile/", null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_hours = models.IntegerChoices()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
